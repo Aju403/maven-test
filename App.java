4750ba09f2838dc8f4c79938d8576d7f4c2b6c5f
Aju403 <arjunbiju403@gmail.com>
2026-01-09 10:25:39 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
-package com.example;
-
-public class App {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Maven GitHub Actions Lab!");
+        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("OS: " + System.getProperty("os.name"));
     }
 }
